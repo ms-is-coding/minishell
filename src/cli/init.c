45e8f3263dae96df0bd6fcbcf585089fb4065f15
M.S <ms@hadronlang.com>
2025-10-07 12:30:39 +0200

    chore(cli): add version and login options

--- src/cli/init.c
+++ src/cli/init.c
@@ -6,7 +6,7 @@
 /*   By: smamalig <smamalig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 12:06:01 by smamalig          #+#    #+#             */
-/*   Updated: 2025/10/02 22:39:46 by smamalig         ###   ########.fr       */
+/*   Updated: 2025/10/05 21:42:09 by smamalig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,8 @@ t_result	cli_init(t_cli *cli, int argc, char **argv)
 	// ft_vector_init(&cli->positional, (size_t)size) != RESULT_OK
 	cli_add(cli, "posix", 0, 0);
 	cli_add(cli, "verbose", 'v', 0);
+	cli_add(cli, "version", 0, 0);
+	cli_add(cli, "login", 0, 0);
 	cli_add(cli, NULL, 'c', 1);
 	cli_init_debug(cli);
 	if (cli_parse(cli, argc, argv) != RESULT_OK)
