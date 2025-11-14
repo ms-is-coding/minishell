a7c1942079d0a7fa4063849e59535469b50a8cc9
M.S <ms@hadronlang.com>
2025-11-14 11:38:59 +0100

    fix(parser): check opcode mask to avoid double EXEC

--- src/parser/list.c
+++ src/parser/list.c
@@ -6,7 +6,7 @@
 /*   By: smamalig <smamalig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 18:20:21 by smamalig          #+#    #+#             */
-/*   Updated: 2025/10/09 09:40:23 by smamalig         ###   ########.fr       */
+/*   Updated: 2025/11/14 11:34:08 by smamalig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_result	parse_semi(t_parser *p, t_token token)
 	if (p->curr_token.type == TOK_EOF
 		|| p->curr_token.type == TOK_PAREN_CLOSE)
 		return (RESULT_OK);
-	if (p->program.data[p->program.len - 1] == OP_EXEC)
+	if ((p->program.data[p->program.len - 1] & OPCODE_MASK) == OP_EXEC)
 		p->program.data[p->program.len - 1] |= EXEC_WAIT_BIT;
 	else
 		program_write_u8(&p->program, OP_EXEC);
