94efe584457d13497f04098998c5ddee215068c7
M.S <ms@hadronlang.com>
2025-11-14 11:06:37 +0100

    chore(expander): add function signature to header

--- include/expander/expander_internal.h
+++ include/expander/expander_internal.h
@@ -6,7 +6,7 @@
 /*   By: smamalig <smamalig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 16:12:11 by smamalig          #+#    #+#             */
-/*   Updated: 2025/10/23 19:37:31 by smamalig         ###   ########.fr       */
+/*   Updated: 2025/11/07 06:27:59 by smamalig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ void	expander_dquote(t_expander *exp, t_var_expansion_mode mode);
 void	expander_dquote_no_var(t_expander *exp, t_var_expansion_mode mode);
 void	expander_char(t_expander *exp, t_var_expansion_mode mode);
 void	expander_user(t_expander *exp, t_var_expansion_mode mode);
+
+void	expander_expand_quotes(t_expander *exp, t_var_expansion_mode mode);
 
 void	expander_var(t_expander *exp, t_var_expansion_mode mode);
 void	expander_var_extract(t_expander *exp, t_var_expansion_mode mode);
