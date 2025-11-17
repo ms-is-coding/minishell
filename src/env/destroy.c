723dee88ad9502c2ece68aeb347cefb0b8856d9d
M.S <ms@hadronlang.com>
2025-11-17 23:26:40 +0100

    chore(env): simplify destroy function

--- src/env/destroy.c
+++ src/env/destroy.c
@@ -6,30 +6,33 @@
 /*   By: smamalig <smamalig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/07 06:32:47 by smamalig          #+#    #+#             */
-/*   Updated: 2025/11/14 11:23:59 by smamalig         ###   ########.fr       */
+/*   Updated: 2025/11/14 15:09:20 by smamalig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env/env.h"
 
-// we assume that key and value are in a single allocation
+static void	env_free_bucket(t_env_bucket *bucket)
+{
+	if (!bucket->key || bucket->flags & ENV_FLAG_STACK)
+		return ;
+	if (bucket->flags & ENV_FLAG_STACK_KEY)
+	{
+		allocator_free_ptr((void *)(intptr_t)bucket->value);
+		return ;
+	}
+	allocator_free_ptr((void *)(intptr_t)bucket->key);
+}
+
+// we assume that key and value are stored in a single allocation
 void	env_destroy(t_env *env)
 {
-	size_t			i;
-	t_env_bucket	*bucket;
+	size_t	i;
 
 	i = -1ul;
 	while (++i < env->capacity)
 	{
-		bucket = &env->buckets[i];
-		if (!bucket->key || bucket->flags & ENV_FLAG_STACK)
-			continue ;
-		if (bucket->flags & ENV_FLAG_STACK_KEY)
-		{
-			allocator_free_ptr((void *)bucket->value);
-			continue ;
-		}
-		allocator_free_ptr((void *)bucket->key);
+		env_free_bucket(&env->buckets[i]);
 	}
 	allocator_free_ptr(env->buckets);
 	env->capacity = 0;
