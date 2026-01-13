44bec601b4e6e38b6418ad7c902c4bbccbbdad01
M.S <ms@hadronlang.com>
2026-01-13 15:26:29 +0100

    fix(util): prevent segfault if /usr/bin/date is missing

--- src/util/time.c
+++ src/util/time.c
@@ -6,7 +6,7 @@
 /*   By: smamalig <smamalig@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/21 13:22:22 by smamalig          #+#    #+#             */
-/*   Updated: 2026/01/04 17:36:30 by smamalig         ###   ########.fr       */
+/*   Updated: 2026/01/12 17:02:14 by smamalig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ int64_t	time_get(void)
 int64_t	time_get(void)
 {
 	char	*output;
+	char	*temp;
 	int64_t	time;
 	int64_t	ns;
 
@@ -38,10 +39,11 @@ int64_t	time_get(void)
 			(char *)(intptr_t)"/usr/bin/date",
 			(char *)(intptr_t)"+%s %N",
 			NULL});
-	if (!output)
+	output && (temp = ft_strchr(output, ' '));
+	if (!output || !temp)
 		return (0);
 	time = ft_atol_safe(output);
-	ns = ft_atoi_safe(ft_strchr(output, ' '));
+	ns = ft_atoi_safe(temp);
 	time = time * 1000 + ns / 1000000;
 	ft_free(output);
 	return (time);
