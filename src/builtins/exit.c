f2465e52b908f672371440c450f8eabb6caef286
M.S <ms@hadronlang.com>
2026-01-15 12:10:35 +0100

    chore(builtins): remove return builtin (not necessary)

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: mattcarniel <mattcarniel@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 15:01:02 by smamalig          #+#    #+#             */
-/*   Updated: 2026/01/14 17:25:51 by mattcarniel      ###   ########.fr       */
+/*   Updated: 2026/01/15 11:54:32 by smamalig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 #include "builtins/builtins.h"
 #include "core/string.h"
-#include "vector/vector.h"
 
 /**
  * @brief Exits the shell.
@@ -32,19 +31,22 @@ int	builtin_exit(
 	char **argv,
 	__attribute__((unused)) char **envp)
 {
-	long	exit_code;
+	int		exit_code;
 	char	*tmp;
 
 	exit_code = 0;
 	write(2, "exit\n", 5);
 	if (argc == 1)
-		exit_code = (int64_t)vec_get(sh->vm.exit_codes, -1);
-	exit_code = ft_atol_safe(argv[1]);
+	{
+		sh_destroy(sh);
+		_exit(sh->vm.last_exit_code);
+		return (sh->vm.last_exit_code);
+	}
+	exit_code = (int32_t)ft_atol_safe(argv[1]);
 	tmp = argv[1];
 	if (argv[1][0] == '+' || argv[1][0] == '-')
 		tmp = argv[1] + 1;
-	if (argc == 2 && (errno == ERANGE
-			|| tmp[ft_strspn(tmp, "0123456789")] != '\0'))
+	if (errno == ERANGE || tmp[ft_strspn(tmp, "0123456789")] != '\0')
 		return (builtin_error(ctx("exit", argv[1]), ERR_NOT_NUMERIC, 2));
 	if (argc > 2)
 		return (builtin_error(ctx("exit", NULL), ERR_TOO_MANY_ARGS, 1));
